03f749cca7cdb9b42c89b2d3ac3c92f812a2d5ab
Waldi Irawan <waldiirawan127@gmail.com>
2019-01-14 13:16:02 +0700

    update

--- start_turnserver.sh
+++ start_turnserver.sh
@@ -2,4 +2,4 @@
 
 echo "Starting TURN/STUN server"
 
-/usr/bin/turnserver -a -v -L 0.0.0.0 --server-name "${TURN_SERVER_NAME}" --static-auth-secret="${TURN_SECRET}" -p ${TURN_PORT} --min-port ${TURN_PORT_START} --max-port ${TURN_PORT_END} ${TURN_EXTRA}
+/usr/bin/turnserver --no-tls --no-dtls --lt-cred-mech -a -v -L 0.0.0.0 --server-name "${TURN_SERVER_NAME}" --static-auth-secret="${TURN_SECRET}" -p ${TURN_PORT} --min-port ${TURN_PORT_START} --max-port ${TURN_PORT_END} --lt-cred-mech ${TURN_EXTRA}
