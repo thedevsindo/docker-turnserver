c6a0afa89d7d372eeec1fd13f4fed886f48c170f
Waldi Irawan <waldiirawan127@gmail.com>
2019-01-15 14:36:14 +0700

    fix user cred

--- start_turnserver.sh
+++ start_turnserver.sh
@@ -3,4 +3,4 @@
 echo "Starting TURN/STUN server"
 
 TURN_CRED=$(turnadmin -k -u $TURN_SERVER_NAME -r $TURN_REALM -p $TURN_SECRET)
-/usr/bin/turnserver --no-tls --no-dtls --lt-cred-mech -a -v -L 0.0.0.0 --server-name "${TURN_SERVER_NAME}" --static-auth-secret="${TURN_CRED}" -p ${TURN_PORT} --min-port ${TURN_PORT_START} --max-port ${TURN_PORT_END} --lt-cred-mech ${TURN_EXTRA} --realm ${TURN_REALM}
+/usr/bin/turnserver --no-tls --no-dtls --lt-cred-mech -a -v -L 0.0.0.0 --server-name "${TURN_SERVER_NAME}" --user "${TURN_SERVER_NAME}:${TURN_CRED}" -p ${TURN_PORT} --min-port ${TURN_PORT_START} --max-port ${TURN_PORT_END} --lt-cred-mech ${TURN_EXTRA} --realm ${TURN_REALM}
